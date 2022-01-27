2533d405c29ccab3117f2e7dc27a3e4f0764e9b1
Matt Kent <mwkent23@gmail.com>
2022-01-26 23:12:37 -0500

    added max and min mating eval and long range piece types

--- lib/constants.py
+++ lib/constants.py
@@ -7,6 +7,11 @@ WINNING_EVAL = MAX_EVAL / 2
 MIN_EVAL = -MAX_EVAL
 DRAW_EVAL = 0
 
+# Mate in 10
+MAX_MATING_EVAL = MAX_EVAL - 10
+MIN_MATING_EVAL = MIN_EVAL + 10
+
 PIECE_TYPES_TO_ROUGH_VALUES = {chess.PAWN: 100, chess.KNIGHT: 300, chess.BISHOP: 300,
                                chess.ROOK: 500, chess.QUEEN: 900, chess.KING: 10_000}
 
+LONG_RANGE_PIECE_TYPES = {chess.BISHOP, chess.ROOK, chess.QUEEN}
